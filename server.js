23cf6b112153993ddfc07ebfd744698a1f7d0382
gadmregon <jim231998@gmail.com>
2022-02-27 18:28:00 +0300

    Server is heroku test

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3500;
-const bodyParser = require("body-parser");
+const mysql = require("mysql2");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
+app.get("/", (req, res) => {
+  res.status(200).json("Сервер работает");
+});
 app.listen(port, () => {
   console.log(`App listen on port ${port}`);
 });
